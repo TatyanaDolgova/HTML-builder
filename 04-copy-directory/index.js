f2f74921cf60eb5e730ae1965a586a7dfad3347d
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-01-22 10:05:55 +0500

    fix: fix bug in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,14 +4,19 @@ const fs = require('fs');
 const folderPath = (path.join(__dirname, 'files'));
 const newFolderPath = (path.join(__dirname, 'files-copy'));
 
-fs.mkdir(newFolderPath, { recursive: true, force: true }, () => {
-  fs.readdir(folderPath, (err, filenames) => {
-    for (let filename of filenames) {
-      const filePath = (path.join(folderPath, filename));
-      const newFilePath = (path.join(newFolderPath, filename));
-      fs.copyFile(filePath, newFilePath, (err) => {
-        if (err) throw err;
-      });
-    }
-  })
-});
+fs.rm(newFolderPath, { recursive: true }, () => {
+
+  fs.mkdir(newFolderPath, { recursive: true, force: true }, () => {
+
+    fs.readdir(folderPath, (err, filenames) => {
+
+      for (let filename of filenames) {
+        const filePath = (path.join(folderPath, filename));
+        const newFilePath = (path.join(newFolderPath, filename));
+        fs.copyFile(filePath, newFilePath, (err) => {
+          if (err) throw err;
+        });
+      }
+    })
+  });
+})
